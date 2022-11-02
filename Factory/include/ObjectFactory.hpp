e2dd12e5ee34c99164eee3e4f61796f01c3f8145
Tristan Gremont <tristan.gremont@epitech.eu>
2022-11-02 08:43:37 +0100

    chore: updated from engine new macros for factories

--- Factory/include/ObjectFactory.hpp
+++ Factory/include/ObjectFactory.hpp
@@ -8,27 +8,12 @@
 #ifndef PLUGIN_TEMPLATE_OBJECTFACTORY_HPP
 #define PLUGIN_TEMPLATE_OBJECTFACTORY_HPP
 
-#include "ScriptingAPI/ISerializableObjectFactory.hpp"
+#include "ScriptingAPI/ScriptingApi.hpp"
 #include <unordered_map>
 
-namespace polymorph::engine
-{
-    class ObjectFactory : public ISerializableObjectFactory
-    {
-        public:
-            ~ObjectFactory() override = default;
-
-            std::shared_ptr<ASerializableObject>
-            createS(std::string type, std::shared_ptr<myxmlpp::Node> &data,
-                    Config::XmlComponent &manager) override;
-
-            bool hasType(std::string &type) override;
-            
-            std::unordered_map<std::string, FactoryLambdaS> _factoriesS = {
-                
-            };
-    };
-}
+OBJECT_FACTORY_BEGIN() {
+    
+} OBJECT_FACTORY_END()
 
 
 #endif //PLUGIN_TEMPLATE_OBJECTFACTORY_HPP
--- Factory/include/ScriptFactory.hpp
+++ Factory/include/ScriptFactory.hpp
@@ -16,47 +16,6 @@
 //@Scripts
 //${SCRIPTS_INCLUDES}
 
-namespace polymorph::engine
-{
-    using Initializer = std::shared_ptr<IComponentInitializer>;
-    class ScriptFactory : public IScriptFactory
-    {
-
-////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
-        public:
-            ~ScriptFactory() override = default;
-
-
-//////////////////////--------------------------/////////////////////////
-
-//Builder template : SCRIPT(${SCRIPT_NAME})
-///////////////////////////// PROPERTIES ////////////////////////////////
-        private:
-            using GameObject = polymorph::engine::safe_ptr<polymorph::engine::Entity>;
-            using FactoryLambda = std::function<Initializer (Config::XmlComponent &data, GameObject entity)>;
-            template<typename T>
-            static inline FactoryLambda _make()
-            {
-                return [](Config::XmlComponent &data, GameObject entity) -> Initializer{ return Initializer(new T(data, entity));};
-            }
-
-            const std::map<const std::string, FactoryLambda> _buildables = {
-                    //${BUILDERS}
-            };
-//////////////////////--------------------------/////////////////////////
-
-
-
-/////////////////////////////// METHODS /////////////////////////////////
-        public:
-            Initializer create(std::string &type, Config::XmlComponent &data,
-                               GameObject entity) override;
-            
-            bool hasType(std::string &type) final;
-//////////////////////--------------------------/////////////////////////
-
-    };
-}
-
-
-
+FACTORY_BEGIN() {
+    
+} FACTORY_END()
